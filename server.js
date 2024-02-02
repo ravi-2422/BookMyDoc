b498414e6d55f228e6f93c1bb534566e6574de3c
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2024-02-02 15:17:08 +0530

    added build folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app  = express();
 const connectDatabase = require('./config/dbConfig');
 app.use(express.json());
@@ -11,6 +12,13 @@ const userRouter = require('./routes/userRoutes');
 const adminRouter = require('./routes/adminRoutes');
 const doctorRouter = require('./routes/doctorRoutes');
 
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, './client/build')));
+
+// Handle other routes by serving the index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
