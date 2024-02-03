8a0fe037b25aca0a205caceb4ce7a86bc309df5a
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2024-02-03 13:51:38 +0530

    remove bug

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require('path');
 const app  = express();
 const connectDatabase = require('./config/dbConfig');
 app.use(express.json());
@@ -12,17 +11,9 @@ const userRouter = require('./routes/userRoutes');
 const adminRouter = require('./routes/adminRoutes');
 const doctorRouter = require('./routes/doctorRoutes');
 
-// Serve static files from the build folder
-app.use(express.static(path.join(__dirname, './client/build')));
-
-// Handle other routes by serving the index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
-
 
 const PORT = process.env.PORT || 5000;
 
