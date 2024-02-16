360ea169ad83e59161d4cb3bdacdacfec509e386
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2024-02-16 23:22:14 +0530

    testing api

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use("/api/doctor", doctorRouter);
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
     console.log("Server is running on Port:", PORT);
 })
